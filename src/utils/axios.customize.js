21786f2bc8a4b14ad912f1650a214474e400d139
mungpham <mungpham919@gmail.com>
2026-01-29 01:35:35 +0700

    change brach to main

---
+++ src/utils/axios.customize.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+});
+
+// Alter defaults after instance has been created
+// instance.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before the request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with the request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lies within the range of 2xx causes this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that fall outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
